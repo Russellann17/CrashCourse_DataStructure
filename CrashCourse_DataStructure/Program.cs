7d9bc0ea965452f8cea585267106def294bbbab9
Russellann17 <dacalrussell@gmail.com>
2024-09-24 16:07:43 +0800

    Changes

--- CrashCourse_DataStructure/Program.cs
+++ CrashCourse_DataStructure/Program.cs
@@ -10,8 +10,33 @@ namespace CrashCourse_DataStructure
     {
         static void Main(string[] args)
         {
-            Lesson2.L2_ClassesAndObjects();
-            Lesson3.L3_LinkedList();
+            Console.WriteLine("[1] Lesson 2 : Classes and Objects" +
+                              "[2] Lesson 3 : Linked List" +
+                              "[0] Exit");
+
+            while (true)
+            {
+                Console.Write("Choose a Lesson: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Lesson2.L2_ClassesAndObjects();
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Lesson3.L3_LinkedList();
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+
+                    case 0:
+                        return;
+                }
+                
+                
+            }
         }
     }
 
