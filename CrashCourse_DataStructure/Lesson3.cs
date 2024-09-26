6411e960b9a2fecb067e74e8919fdb85fea20454
Russellann17 <dacalrussell@gmail.com>
2024-09-26 12:25:58 +0800

    Lesson 3

--- CrashCourse_DataStructure/Lesson3.cs
+++ CrashCourse_DataStructure/Lesson3.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Runtime.Remoting.Messaging;
 
 namespace CrashCourse_DataStructure
 {
     class Lesson3
     {
         public static void L3_LinkedList()
-        {  
+        {
             ShoppingList shoppingList = new ShoppingList();
             string input;
+
             while (true)
             {
-                
                 Console.WriteLine("--- SHOPPING LIST MANAGER ---" +
                                   "\n[1] Add Item" +
                                   "\n[2] Remove Item" +
@@ -24,15 +23,19 @@ namespace CrashCourse_DataStructure
                     case 1:
                         while (true)
                         {
-                            Console.Write("Enter an item to add to the list (or type 'exit' to finish):");
+                            Console.Write("Enter an item to add to the list (or type 'exit' to finish): ");
                             input = Console.ReadLine();
                             if (input.ToLower() == "exit") break;
-                            shoppingList.AddItem(input);
+
+                            Console.Write("Enter the index where you want to add the item: ");
+                            int index = Convert.ToInt32(Console.ReadLine());
+                            shoppingList.AddItem(input, index);
                         }
                         Console.WriteLine("Item/s added successfully.\n");
                         shoppingList.DisplayItems();
-                        Console.WriteLine("Total number of items : " + shoppingList.CountItems() + "\n");
+                        Console.WriteLine("Total number of items: " + shoppingList.CountItems() + "\n");
                         break;
+
                     case 2:
                         Console.Write("\nEnter an item to remove from the list: ");
                         input = Console.ReadLine();
@@ -41,39 +44,39 @@ namespace CrashCourse_DataStructure
 
                         Console.WriteLine("\nYour updated shopping list: ");
                         shoppingList.DisplayItems();
-                        Console.WriteLine("Total number of items : " + shoppingList.CountItems() + "\n");
+                        Console.WriteLine("Total number of items: " + shoppingList.CountItems() + "\n");
                         break;
+
                     case 3:
                         Console.Write("Search for an item: ");
                         input = Console.ReadLine();
                         if (shoppingList.CheckItem(input)) Console.WriteLine("Yes, " + input + " is in your shopping list.\n");
                         else Console.WriteLine("No, " + input + " is not in your shopping list.\n");
                         break;
+
                     case 4:
                         return;
-
                 }
             }
         }
-        
     }
 
     class ItemNode
     {
         public string item { get; set; }
-        public ItemNode next {  get; set; }
+        public ItemNode next { get; set; }
 
         public ItemNode(string item)
         {
             this.item = item;
             next = null;
         }
-
     }
 
     class ShoppingList
     {
         public ItemNode priority;
+
         public int CountItems()
         {
             int count = 0;
@@ -88,33 +91,39 @@ namespace CrashCourse_DataStructure
             return count;
         }
 
-        public void AddItem(string item)
+        public void AddItem(string item, int index)
         {
             ItemNode newItem = new ItemNode(item);
-            if(priority == null)
+            if (index <= 0 || priority == null)
             {
+                newItem.next = priority;
                 priority = newItem;
             }
             else
             {
                 ItemNode current = priority;
-                while (current.next != null)
+                int currentIndex = 0;
+
+                while (currentIndex < index - 1 && current.next != null)
                 {
                     current = current.next;
+                    currentIndex++;
                 }
+
+                newItem.next = current.next;
                 current.next = newItem;
             }
         }
-        
+
         public void DisplayItems()
         {
             ItemNode current = priority;
             int count = 1;
 
             Console.WriteLine("Your shopping list:");
-            while(current != null)
+            while (current != null)
             {
-                Console.WriteLine(count + ". "+ current.item);
+                Console.WriteLine(count + ". " + current.item);
                 current = current.next;
                 count++;
             }
@@ -139,16 +148,16 @@ namespace CrashCourse_DataStructure
 
             if (current.next != null)
             {
-                current.next = current.next.next; 
+                current.next = current.next.next;
             }
         }
 
-        public bool CheckItem(string Item)
+        public bool CheckItem(string item)
         {
             ItemNode current = priority;
             while (current != null)
             {
-                if (current.item.Equals(Item, StringComparison.OrdinalIgnoreCase)) return true;
+                if (current.item.Equals(item, StringComparison.OrdinalIgnoreCase)) return true;
                 current = current.next;
             }
             return false;
